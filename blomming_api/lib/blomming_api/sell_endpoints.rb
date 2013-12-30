d136d0d76139622e91dea7b394e4aac977778a64
Giorgio Robino <giorgio.robino@gmail.com>
2013-12-30 15:25:44 +0100

    v.0.3.9

--- blomming_api/lib/blomming_api/sell_endpoints.rb
+++ blomming_api/lib/blomming_api/sell_endpoints.rb
@@ -8,32 +8,135 @@ module BlommingApi
     #
     # PAYMENT_TYPES
     #
-    # TODO
+    def sell_payment_types_read (params={})
+      url = api_url "/sell/payment_types​/user_list"
+      req = request_params(params)
+
+      load_or_retry do
+        RestClient.get url, req
+      end  
+    end
+
+    def sell_payment_types_create (payload, params={})
+      url = api_url "/sell/payment_types​/new"
+      load = MultiJson.dump payload
+      req = request_params(params)
+
+      load_or_retry do
+        RestClient.post url, load, req 
+      end  
+    end
+
+    def sell_payment_types_update (id, payload, params={})
+      url = api_url "/sell/payment_types​/#{id}"
+      load = MultiJson.dump payload
+      req = request_params(params)
+
+      load_or_retry do
+        RestClient.put url, load, req 
+      end  
+    end
+
+    def sell_payment_types_delete (id, params={})
+      url = api_url "/sell/payment_types​/#{id}"
+      req = request_params(params)
+
+      load_or_retry do
+        RestClient.delete url, req 
+      end  
+    end
 
     #
     # REGISTER
     #
-    # TODO
+    def sell_register (payload, params={})
+      url = api_url "/sell/register"
+      load = MultiJson.dump payload
+      req = request_params(params)
+
+      load_or_retry do
+        RestClient.post url, load, req 
+      end  
+    end
 
     #
     # SHIPPING_COUNTRIES
     #
-    # TODO
+    def sell_shipping_countries_all (params={})
+      url = api_url "/sell/shipping_countries​​/all"
+      req = request_params({locale: @locale}.merge(params))
+
+      load_or_retry do
+        RestClient.get url, req
+      end  
+    end
 
     #
     # SHIPPING_PROFILES
     #
-    # TODO
+=begin    
+    def sell_payment_shipping_profiles_read (params={})
+      url = api_url "/sell/shipping_profiles"
+      req = request_params(params)
+
+      load_or_retry do
+        RestClient.get url, req
+      end
+    end
+
+    def sell_shipping_profiles_create (payload, params={})
+      url = api_url "/sell/shipping_profiles/new"
+      load = MultiJson.dump payload
+      req = request_params(params)
+
+      load_or_retry do
+        RestClient.post url, load, req 
+      end  
+    end
+
+    def sell_shipping_profiles_update (id, payload, params={})
+      url = api_url "/sell/shipping_profiles/#{id}"
+      load = MultiJson.dump payload
+      req = request_params(params)
+
+      load_or_retry do
+        RestClient.put url, load, req 
+      end  
+    end
+
+    def sell_shipping_profiles_delete (id, params={})
+      url = api_url "/sell/payment_types​/#{id}"
+      req = request_params(params)
+
+      load_or_retry do
+        RestClient.delete url, req 
+      end  
+    end
+=end
 
     #
     # SHIPPING_REGIONS
     #
-    # TODO
+    def sell_shipping_regions (params={})
+      url = api_url "/sell/shipping_regions​/all"
+      req = request_params({locale: @locale}.merge(params))
+
+      load_or_retry do
+        RestClient.get url, req
+      end  
+    end
 
     #
     # SHOP_DASHBOARD
     #
-    # TODO
+    def sell_shop_dashboard (params={})
+      url = api_url "/sell/shop/dashboard"
+      req = request_params({locale: @locale}.merge(params))
+
+      load_or_retry do
+        RestClient.get url, req
+      end  
+    end
 
     #
     # SHOP_ITEMS
@@ -141,7 +244,14 @@ module BlommingApi
     #
     # SHOP_SHIPPING_PROFILES
     #
-    # TODO
+    def sell_shop_shipping_profiles (params={})
+      url = api_url "/sell/shop/shipping_profiles"
+      req = request_params({locale: @locale}.merge(params))
+
+      load_or_retry do
+        RestClient.get url, req
+      end  
+    end
 
     #
     # SHOP_USER_DETAILS
